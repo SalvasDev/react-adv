b611c1cafee700c6eed1bf0f73838bd5dde9e383
SalvasDev <salsanzmelc@gmail.com>
2023-03-02 19:56:49 -0600

    cambios

---
+++ src/routes/routes.ts
@@ -0,0 +1,31 @@
+import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+
+interface Route {
+  to: string;
+  path: string;
+  Component: () => JSX.Element;
+  name: string;
+}
+
+
+
+export const routes: Route[] = [
+  {
+    to:  '/lazy1',
+    path: 'lazy-1',
+    Component: LazyPage1,
+    name: 'Lazy-1'
+  },
+  {
+    to:  '/lazy2',
+    path: 'lazy-2',
+    Component: LazyPage2,
+    name: 'Lazy-2'
+  },
+  {
+    to:  '/lazy3',
+    path: 'lazy-3',
+    Component: LazyPage3,
+    name: 'Lazy-3'
+  }
+];
